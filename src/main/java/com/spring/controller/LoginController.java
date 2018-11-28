5751df97a491193b2874625e6d71e92b8745acc1
Amey <amey.more@a4technology.com>
2018-11-28 18:32:15 +0530

    logot commit

--- src/main/java/com/spring/controller/LoginController.java
+++ src/main/java/com/spring/controller/LoginController.java
@@ -1,7 +1,18 @@
 package com.spring.controller;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,11 +24,51 @@ public class LoginController {
 		model.addAttribute("msg", "Please Enter Your Login Details");
 		return "login";
 	}
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String adminPage(ModelMap model) {
+       // model.addAttribute("user", getPrincipal());
+        return "admin";
+    }
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String onLogoutSuccess(HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse,
+			Authentication authentication) throws IOException, ServletException {
+		HttpSession session= httpServletRequest.getSession(false);
+	    SecurityContextHolder.clearContext();
+	         session= httpServletRequest.getSession(false);
+	        if(session != null) {
+	            session.invalidate();
+	        }
+	        for(Cookie cookie : httpServletRequest.getCookies()) {
+	            cookie.setMaxAge(0);
+	        }
+
+		
+		if (authentication != null && authentication.getDetails() != null) {
+			try {
+				httpServletRequest.getSession().invalidate();
+				System.out.println("User Successfully Logout");
+				// you can add more codes here when the user successfully logs
+				// out,
+				// such as updating the database for last active.
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+		// redirect to login
+		// httpServletResponse.sendRedirect("/login");
+		return "login";
+	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String submit(Model model, @ModelAttribute("loginBean") LoginBean loginBean) {
-		if (loginBean != null && loginBean.getUserName() != null & loginBean.getPassword() != null) {
-			if (loginBean.getUserName().equals("chandra") && loginBean.getPassword().equals("chandra123")) {
+	public String submit(Model model,
+			@ModelAttribute("loginBean") LoginBean loginBean) {
+		if (loginBean != null && loginBean.getUserName() != null
+				& loginBean.getPassword() != null) {
+			if (loginBean.getUserName().equals("chandra")
+					&& loginBean.getPassword().equals("chandra123")) {
 				model.addAttribute("msg", loginBean.getUserName());
 				return "success";
 			} else {
--- src/main/webapp/WEB-INF/views/success.jsp
+++ src/main/webapp/WEB-INF/views/success.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+	<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -10,5 +11,6 @@
 	<font color="green"><h1>Hello</h1></font>
 	<h2>${msg}</h2> You have successfully logged in.
 	<font color="green"><h1>Welcome to Spring world !</h1></font>
+	<a href="<c:url value="/logout" />" > Logout</a>
 </body>
 </html>
